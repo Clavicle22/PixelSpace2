ac156f88519ff4128db9021ac17a12f02ba6182a
gaëll <gaell.gg@protonmail.com>
2026-01-01 21:44:14 +0100

    Initial Commit

---
+++ PixelSpace/PixelSpaceApp.swift
@@ -0,0 +1,17 @@
+//
+//  PixelSpaceApp.swift
+//  PixelSpace
+//
+//  Created by Gaëll on 01/01/2026.
+//
+
+import SwiftUI
+
+@main
+struct PixelSpaceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
